8c963c69849cd0386c62662ecf74ae941c13902e
j-rapkin <jc.rapkin@gmail.com>
2020-09-22 15:11:35 -0500

    Created CustomList class. Created a constructor that sets the default count, array, and array capacity. This passes the corresponding unit test.

---
+++ Custom_List/CustomList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom_List
+{
+	public class CustomList<T> : IEnumerable
+	{
+		private int count;
+		private int arrayCapacity;
+		private T[] arrayOfElements;
+
+		public IEnumerator GetEnumerator()
+		{
+			for (int index=0; index < count; index++)
+			{
+				yield return arrayOfElements[index];
+			}
+		}
+		public T this[int index]
+		{
+			get
+			{
+				if(index >= count)
+				{
+					throw new ArgumentOutOfRangeException();
+				}
+				else
+				{
+					return arrayOfElements[index];
+				}
+			}
+			set
+			{
+				if (index >= count)
+				{
+					throw new ArgumentOutOfRangeException();
+				}
+				else
+				{
+					arrayOfElements[index] = value;
+				}
+			}
+		}
+		public int Count
+		{
+			get
+			{
+				return count;
+			}
+		}
+		public int Capacity
+		{
+			get
+			{
+				return arrayCapacity;
+			}
+		}
+		public CustomList()
+		{
+			count = 0;
+			arrayCapacity = 4;
+			arrayOfElements = new T[arrayCapacity];
+		}
+
+	}
+}
